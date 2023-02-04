f9b6cb05b96c4ce40bf625c54fefd0099f17f422
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-04 12:53:40 +0530

    How to Play Sounds on a Website

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ var numberOfDrumButtons = document.querySelectorAll(".drum").length;
 
 for (var i=0; i<numberOfDrumButtons; i++) {
     document.querySelectorAll("button")[i].addEventListener('click', function () {
-        alert("I got clicked!")
+
+        this.style.color="white"
+
     });
 }
+
+// var audio = new Audio("sounds/tom-1.mp3");
+// audio.play();
