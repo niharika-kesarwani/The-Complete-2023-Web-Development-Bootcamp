f2b2bf152cc5c7ec6f6fbf90c5943292da225cef
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-08 17:51:34 +0530

    Check Which Button is Pressed

--- game.js
+++ game.js
@@ -2,6 +2,18 @@ var buttonColours = ["red", "blue", "green", "yellow"];
 
 var gamePattern = [];
 
+var userClickedPattern = [];
+
+$(".btn").click(function () {
+
+    var userChosenColour = $(this).attr("id");
+
+    userClickedPattern.push(userChosenColour);
+
+    console.log(userClickedPattern);
+    
+});
+
 function nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
