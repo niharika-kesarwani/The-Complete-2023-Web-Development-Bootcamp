1c6f6029d35a78d4f82bfc03951f36fa6e72a03c
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-08 17:28:30 +0530

    Show Sequence to the User with Animations & Sounds

--- game.js
+++ game.js
@@ -5,8 +5,11 @@ var gamePattern = [];
 function nextSequence() {
 
     var randomNumber = Math.floor(Math.random() * 4);
-
     var randomChosenColor = buttonColours[randomNumber];
-
     gamePattern.push(randomChosenColor);
+
+    $("#" + randomChosenColor).fadeIn(100).fadeOut(100).fadeIn(100);
+
+    var audio = new Audio("sounds/" + randomChosenColor + ".mp3");
+    audio.play();
 }
