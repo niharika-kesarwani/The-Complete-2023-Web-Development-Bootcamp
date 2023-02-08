38052e91e56f93ca12400377b47477eb16aff754
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-08 17:14:37 +0530

    Create A New Pattern

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### The Complete 2023 Web Development Bootcamp
 
-Simon Game Challenge Live Link - 
+Simon Game Challenge Live Link - https://simon-game-challenge-niharika.netlify.app/
--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-alert("Hello");
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+
+function nextSequence() {
+
+    var randomNumber = Math.floor(Math.random() * 4);
+
+    var randomChosenColor = buttonColours[randomNumber];
+
+    gamePattern.push(randomChosenColor);
+}
