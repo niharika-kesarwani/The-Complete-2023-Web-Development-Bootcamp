8c43db87bd7834fa2d039a726a4e5e10417cdf5c
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-13 16:52:42 +0530

    Solution to the BMI Routing Challenge

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ### The Complete 2023 Web Development Bootcamp
 
-Command to run this project - node calculator.js
+Command to run the Calculator project - node calculator.js && http://localhost:3000/
+
+Command to run the BMI Calculator project - node calculator.js && http://localhost:3000/bmicalculator
--- calculator.js
+++ calculator.js
@@ -20,6 +20,20 @@ app.post("/", function(req, res) {
    res.send("The result of the calculation is " + result)
 })
 
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+})
+
+app.post("/bmicalculator", function (req, res) {
+
+    var weight = parseFloat(req.body.weight);
+    var height = parseFloat(req.body.height);
+
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
+
 app.listen(3000, function() {
     console.log("Server started at port 3000!")
 });
