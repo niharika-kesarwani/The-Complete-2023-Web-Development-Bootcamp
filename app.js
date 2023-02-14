549809680098510af244b9f3be7f1866f18bea5e
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-14 23:20:20 +0530

    Express to Render a Website with Live API Data

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ app.get("/", function (req, res) {
             const weatherData = JSON.parse(data);
             const temp = weatherData.main.temp;
             const weatherDescription = weatherData.weather[0].description;
-            console.log(temp, weatherDescription);
-            
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            res.setHeader("Content-Type", "text/html");
+            res.write("The weather is currently " + weatherDescription + "<p>");
+            res.write("<h1>The temperature in Nashik is " + temp + " degrees Celcius</h1>");
+            res.write("<img src=" + imageURL + ">");
+            res.send();
         })
     })
-
-    res.send("Server is up and running!")
 })
 
 app.listen(3000, function () {
-    console.log("Server is running on port 3000.")
+    console.log("Server is running on port 3000.");
 })
