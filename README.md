30b747703440bb7c4549544c000de0d0f37e5705
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-15 01:45:29 +0530

    Setting Up the Sign Up Page

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 ### The Complete 2023 Web Development Bootcamp
 
-Command to run the Weather Project - node app.js && http://localhost:3000/
+Command to run the Newsletter-Signup Project - node app.js && http://localhost:3000/
--- app.js
+++ app.js
@@ -1,40 +1,26 @@
+//jshint esversion: 6
+
 const express = require("express");
-const https = require("https");
 const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/signup.html");
 });
 
 app.post("/", function(req, res) {
-    const query = req.body.cityName;
-    const apiKey = "c38d5e4f757ff2f9641cc92635828e8b";
-    const unit = "metric";
-    const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + apiKey + "&units=" + unit;
+    var firstName = req.body.fName;
+    var lastName = req.body.lName;
+    var email = req.body.email;
 
-    https.get(url, function (response) {
-        console.log(response.statusCode);
-
-        response.on("data", function (data) {
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            const cityName = weatherData.name;
-            res.setHeader("Content-Type", "text/html");
-            res.write("The weather is currently " + weatherDescription + "<p>");
-            res.write("<h1>The temperature in " + cityName + " is " + temp + " degrees Celcius</h1>");
-            res.write("<img src=" + imageURL + ">");
-            res.send();
-        })
-    })
+    console.log(firstName, lastName, email);
 });
 
-app.listen(3000, function () {
-    console.log("Server is running on port 3000.");
+app.listen(3000, function() {
+    console.log("Server is running on port 3000!");
 });
