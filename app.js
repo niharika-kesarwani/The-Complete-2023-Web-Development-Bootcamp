a179340fae1c435df57bb87023008c842b2ddb6d
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-16 17:33:48 +0530

    Posting Data to Mailchimp's Servers via their API

--- app.js
+++ app.js
@@ -3,24 +3,70 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 const app = express();
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(__dirname + "/signup.html");
 });
 
-app.post("/", function(req, res) {
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var email = req.body.email;
+app.post("/", function (req, res) {
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, email);
+    const data = {
+        members: [{
+            email_address: email,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: firstName,
+                LNAME: lastName
+            }
+        }]
+    };
+
+    const jsonData = JSON.stringify(data);
+
+    const url = "https://us12.api.mailchimp.com/3.0/lists/4e82e091a1";
+
+    const options = {
+        method: "POST",
+        auth: "niharika:c8dfee87f31e528021faf952a21f922b-us12"
+    }
+
+    const request = https.request(url, options, function(response) {        
+        response.on("data", function(data) {
+            console.log(JSON.parse(data));
+        });
+    })
+
+    request.write(jsonData);
+    request.end();
+
 });
 
-app.listen(3000, function() {
+// {
+//     "email_address": "$user_email",
+//     "status": "subscribed",
+//     "merge_fields": {
+//         "FNAME": "$user_fname",
+//         "LNAME": "$user_lname"
+//     }
+// }
+
+app.listen(3000, function () {
     console.log("Server is running on port 3000!");
 });
+
+// API Key
+// c8dfee87f31e528021faf952a21f922b-us12
+
+// List ID
+// 4e82e091a1
