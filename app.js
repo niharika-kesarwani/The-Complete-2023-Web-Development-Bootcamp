ceb70ff79b7cd83ac7d55b0de596eaba1a84c6e2
NiharikaNeha <kesarwani.neha9@gmail.com>
2023-02-24 21:26:43 +0530

    updated env file

--- app.js
+++ app.js
@@ -77,5 +77,5 @@ app.post("/failure", function (req, res) {
 // }
 
 app.listen(process.env.PORT || 3000, function () {
-  console.log("Server is running on port 3000!");
+  console.log(`Server is running on port ${process.env.PORT || 3000}!`);
 });
