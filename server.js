5470726e2fe15ae365947d11a1bc2af6ecfd0ebc
Niharika Kesarwani <45658446+niharika-kesarwani@users.noreply.github.com>
2025-06-02 18:59:41 +0530

    fix: add cors

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
+
+app.use(cors());
 
 app.get("/", async (req, res) => {
   const { q } = req.query;
